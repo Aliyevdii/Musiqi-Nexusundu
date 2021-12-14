f893b8aab26bb1cba91fa5c087a880634890bdf2
Aliyevdii <95511686+Aliyevdii@users.noreply.github.com>
2021-12-14 20:23:39 +0400

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -21,15 +21,15 @@ reply_markup=keyboard
                 ],
                 [
                     InlineKeyboardButton(
-                        "🏷️ Destek Grubu", url="https://t.me/SohbetOdagi"
+                        "🌐 Support Grubu", url="https://t.me/NEXUS_MMC"
                     )
                 ],
                 [
                     InlineKeyboardButton(
-                        "🔧 Geliştirici", url = "https://t.me/Bir_Beyfendi"
+                        "👨🏻‍💻 Sahibi", url = "https://t.me/A_l_i_y_e_v_d_i"
                     ),
                     InlineKeyboardButton(
-                        "🔊 Asistan" , url = "https://t.me/HerTeldenAsistan"
+                        "👨🏻‍🎤 Asistan" , url = "https://t.me/NexusAsistan"
                     )
                 ],
                 [ 
@@ -37,7 +37,7 @@ reply_markup=keyboard
                         "🌀 Komutlar" , url = "https://telegra.ph/Komutlar-10-22"
                     ),
                     InlineKeyboardButton(
-                        "🎮 Oyun Botu", url="https://t.me/BasitOyunBot"
+                        "💭 Sohbet Gurupmuz", url="https://t.me/iron_Blood_Gurup"
                     )
                 ]
             ]
@@ -47,12 +47,12 @@ reply_markup=keyboard
 
 @Client.on_message(command(["bilgi", f"bilgi@{BOT_USERNAME}"]))
 async def bilgi(_, message: Message):
-      await message.reply_text(f"**Selam {message.from_user.mention}!\n Bu botun bilgi menüsü 🤩\n\n ▶️ /oynat - şarkı çalmak için youtube url'sine veya şarkı dosyasına yanıt verme\n ▶️ /oynat <song name> - istediğiniz şarkıyı çal\n 🔴 /ytp <Sorgu> - youtube üzerinden çalma\n 🎵 /bul <song name> - istediğiniz şarkıları hızlı bir şekilde bulun\n 🎵 /vbul istediğiniz videoları hızlı bir şekilde bulun\n 🔍 /ara <query> - youtube'da ayrıntıları içeren videoları arama\n\n Yalnızca yöneticiler için..\n ▶️ /devam - şarkı çalmaya devam et\n ⏹ /bitir - müzik çalmayı durdurma\n 🔼 /ver botun sadece yönetici için kullanılabilir olan komutlarını kullanabilmesi için kullanıcıya yetki ver\n 🔽 /al botun yönetici komutlarını kullanabilen kullanıcının yetkisini al\n 🎚 /ses asistan hesabın ses seviyesini kontrol et\n\n ⚪ /katil - Müzik asistanı grubunuza katılır\n ⚫ /ayril - Müzik asistanı grubunuzu terk eder.\n\n ❗ Not:\n Botun aktif çalışması için şu üç yetkiye ihtiyaç vardır:\n - Mesaj silme yetkisi,\n - Bağlantı ile davet etme yetkisi,\n - Sesli sohbeti yönetme yetkisi.**", 
+      await message.reply_text(f"**Selam {message.from_user.mention}!\n Bu botun bilgi menüsü 🤓\n\n ▶️ /oynat - şarkı çalmak için youtube url'sine veya şarkı dosyasına yanıt verme\n ▶️ /oynat <song name> - istediğiniz şarkıyı çal\n 🔴 /ytp <Sorgu> - youtube üzerinden çalma\n 🎵 /bul <song name> - istediğiniz şarkıları hızlı bir şekilde bulun\n 🎵 /vbul istediğiniz videoları hızlı bir şekilde bulun\n 🔍 /ara <query> - youtube'da ayrıntıları içeren videoları arama\n\n Yalnızca yöneticiler için..\n ▶️ /devam - şarkı çalmaya devam et\n ⏹ /bitir - müzik çalmayı durdurma\n 🔼 /ver botun sadece yönetici için kullanılabilir olan komutlarını kullanabilmesi için kullanıcıya yetki ver\n 🔽 /al botun yönetici komutlarını kullanabilen kullanıcının yetkisini al\n 🎚 /ses asistan hesabın ses seviyesini kontrol et\n\n ⚪ /katil - Müzik asistanı grubunuza katılır\n ⚫ /ayril - Müzik asistanı grubunuzu terk eder.\n\n ❗ Not:\n Botun aktif çalışması için şu üç yetkiye ihtiyaç vardır:\n - Mesaj silme yetkisi,\n - Bağlantı ile davet etme yetkisi,\n - Sesli sohbeti yönetme yetkisi.**", 
       reply_markup=InlineKeyboardMarkup(
              [
                  [
                      InlineKeyboardButton(
-                         "⚙ Geliştirici", url="https://t.me/Bir_Beyfendi")
+                         "👨🏻‍💻 Sahibi", url="https://t.me/A_l_i_y_e_v_d_i")
                  ]
              ]
          )
